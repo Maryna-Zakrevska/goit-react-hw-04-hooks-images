44a26340fcd94b9ef604cb13f5b913cf8fe667df
Maryna-Zakrevska <lovemama.com.ua@gmail.com>
2022-01-26 19:27:35 +0200

    das

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,12 +1,11 @@
 import React from "react";
-import { Triangle } from "react-loader-spinner";
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import { Puff } from 'react-loading-icons'
 import {LoaderDivStyled} from "./Loader.styled"
 
 export const Loader = () => {
   return (
     <LoaderDivStyled>
-      <Triangle color="#3f51b5" height={200} width={200} />
+      <Puff stroke="#3f51b5" strokeOpacity={.125} speed={.75} />
     </LoaderDivStyled>
   );
 };
--- src/components/Loader/Loader.styled.js
+++ src/components/Loader/Loader.styled.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 
 const LoaderDivStyled = styled.div`
   display: flex;
+  width: 300px;
+  height: 300px;
   justify-content: center;
   width: 100%;
 `;
